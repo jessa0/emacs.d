e7b2e32761abcbb0ca08d30db616e7e7b597406a
Jessa <git@jessa0.com>
2021-07-18 11:47:26 -0700

    add treemacs integration packages

--- lisp/geogriffin-custom.el
+++ lisp/geogriffin-custom.el
@@ -167,7 +167,7 @@
      ("melpa" . "https://melpa.org/packages/")))
  '(package-enable-at-startup t)
  '(package-selected-packages
-   '(delight ripgrep lsp-java persistent-scratch undohist undo-propose lua-mode typescript-mode vscode-icon dired-narrow dired-ranger dired-sidebar dired-subtree bash-completion company yaml-mode forge projectile flx-ido toml-mode erlang flycheck-inline cargo rust-mode rust-playground which-key use-package magit-libgit lsp-ui magit-popup flycheck markdown-mode highlight-symbol magit))
+   '(delight treemacs-magit treemacs-projectile ripgrep lsp-java persistent-scratch undohist undo-propose lua-mode typescript-mode vscode-icon dired-narrow dired-ranger dired-sidebar dired-subtree bash-completion company yaml-mode forge projectile flx-ido toml-mode erlang flycheck-inline cargo rust-mode rust-playground which-key use-package magit-libgit lsp-ui magit-popup flycheck markdown-mode highlight-symbol magit))
  '(persistent-scratch-autosave-interval 30)
  '(persistent-scratch-autosave-mode t)
  '(persistent-scratch-backup-directory nil)
